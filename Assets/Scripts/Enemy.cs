e7ece3997a70e7ab8c126c237f34ef51d69483f9
vladtop1999 <vladhusaiov@gmail.com>
2020-03-21 02:40:36 +0100

    Add project

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MovingObject
+{
+    public AudioClip enemyAttack1;
+    public AudioClip enemyAttack2;
+
+    public int playerDamage;
+
+    private Animator animator;
+    private Transform target;
+    private bool skipMove;
+
+    // Start is called before the first frame update
+   protected override void Start()
+    {
+        //Register this enemy with our instance of GameManager by adding it to a list of Enemy objects. 
+        //This allows the GameManager to issue movement commands.
+        GameManager.instance.AddEnemyToList(this);
+
+        //Get and store a reference to the attached Animator component.
+        animator = GetComponent<Animator>();
+
+        //Find the Player GameObject using it's tag and store a reference to its transform component.
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        //Call the start function of our base class MovingObject.
+        base.Start();
+    }
+
+
+
+    protected override void AttemptMove<T>(int xDir, int yDir)
+    {
+        if (skipMove)
+        {
+            skipMove = false;
+            return;
+        }
+
+        base.AttemptMove<T>(xDir, yDir);
+        skipMove = true;
+    }
+
+    public void MoveEnemy()
+    {
+        int xDir = 0;
+        int yDir = 0;
+
+        if (Mathf.Abs(target.position.x - transform.position.x) < float.Epsilon)
+        {
+            yDir = target.position.y > transform.position.y ? 1 : -1;
+        }
+        else
+        {
+            xDir = target.position.x > transform.position.x ? 1 : -1;
+        }
+        AttemptMove<Player>(xDir, yDir);
+    }
+
+
+    protected override void OnCantMove<T>(T component)
+    {
+
+        Player hitPlayer = component as Player;
+       
+
+        hitPlayer.LoseFood(playerDamage);
+        animator.SetTrigger("enemyAttack");
+        SoundManager.instance.RandomizeSfx(enemyAttack1, enemyAttack2);
+
+    }
+}
